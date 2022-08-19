ac3c9b7ef39a3a4a1940a9e59006ccd87dddd268
JunIce <east.qrj@gmail.com>
2022-08-19 08:36:55 +0800

    chore(vue2): rename bundle name

--- packages/site/src/main.js
+++ packages/site/src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue';
 import App from './App.vue';
 import Comps from '@junice/vue2';
 import router from './router';
-import '@junice/vue2/dist/rj_12.vue.lib.css';
+import '@junice/vue2/dist/junice.vue2.css';
 import './styles/index.scss';
 
 Vue.use(Comps);
