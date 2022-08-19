8553ab94e2127b2d7b6301d9c414aff1ee4b2414
JunIce <east.qrj@gmail.com>
2022-08-19 09:52:30 +0800

    docs(site): add doc url

--- packages/site/site.config.js
+++ packages/site/site.config.js
@@ -4,4 +4,9 @@ module.exports = [
     path: '/component/side-tree',
     module: () => import('@junice/vue2/src/components/side-tree/demo.md'),
   },
+  {
+    label: 'modalContainer',
+    path: '/component/modal-container',
+    module: () => import('@junice/vue2/src/components/modal-container/demo.md'),
+  },
 ];
