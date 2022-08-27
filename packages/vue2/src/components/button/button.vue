9943994500b3441310cfc92bbf8f1c5a942c3871
volcan0 <east.qrj@gmail.com>
2022-08-27 15:45:57 +0800

    chore(*): vue2 need to do

---
+++ packages/vue2/src/components/button/button.vue
@@ -0,0 +1,50 @@
+<template>
+  <button :class="defaultCls"><slot></slot></button>
+</template>
+
+<script>
+export default {
+  name: 'JButton',
+  props: {
+    type: {
+      type: String,
+      default: 'default',
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+    outline: {
+      type: Boolean,
+      default: false,
+    },
+    round: {
+      type: Boolean,
+      default: false,
+    },
+    size: {
+      type: String,
+      default: '',
+    },
+  },
+  computed: {
+    defaultCls() {
+      return {
+        btn: true,
+        'btn-block': true,
+        'btn-info': this.type === 'info',
+        'btn-default': this.type === 'default',
+        'btn-primary': this.type === 'primary',
+        'btn-success': this.type === 'success',
+        'btn-danger': this.type === 'danger',
+        'btn-warning': this.type === 'warning',
+        disabled: this.disabled,
+        outline: this.outline,
+        'btn-rounded': this.round,
+      };
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped src="@/assets/styles/button.scss"></style>
