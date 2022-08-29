4f19e6359d555bc4ffec4fd1d8d08a353ff4c154
volcan0 <east.qrj@gmail.com>
2022-08-30 06:07:29 +0800

    chore(vite): add resolve alias

--- packages/vue3/vite.config.ts
+++ packages/vue3/vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite';
+import path from 'path';
 import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // root: ,
+  root: './',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
   plugins: [vue()],
 });
