56bf39668eb633048e4003b2377da8260419047e
volcan0 <east.qrj@gmail.com>
2023-01-07 10:46:45 +0800

    Test(Button): add demo test case

--- packages/react/src/Button/Button.test.tsx
+++ packages/react/src/Button/Button.test.tsx
@@ -1,37 +1,15 @@
-import React from "react";
-import { render, within } from "@testing-library/react";
+import React from 'react';
+import { render } from '@testing-library/react';
 
-import TestComponent from "./Button";
-import { TestComponentProps } from "./Button.types";
+import Button from './Button';
+import { BaseButtonProps } from './interface';
 
-describe("TestComponent", () => {
-  const renderComponent = ({ heading, content }: Partial<TestComponentProps>) =>
-    render(
-      <TestComponent
-        heading={heading || "Default heading text"}
-        content={content || <div>Default content</div>}
-      />
-    );
+describe('Button', () => {
+  const renderComponent = ({ type }: Partial<BaseButtonProps>) =>
+    render(<Button type={type}>type</Button>);
 
-  it("should render heading text correctly", () => {
-    const headingText = "Some test heading";
-
-    const { getByTestId } = renderComponent({ heading: headingText });
-
-    const testComponent = getByTestId("test-component__heading");
-
-    expect(testComponent).toHaveTextContent(headingText);
-  });
-
-  it("should render content correctly", () => {
-    const { getByTestId } = renderComponent({
-      content: <div data-testid="some-test-content">I am test content</div>
-    });
-
-    expect(
-      within(getByTestId("test-component__content")).queryByTestId(
-        "some-test-content"
-      )
-    ).toBeInTheDocument();
+  it('should render primary', () => {
+    const { getByRole } = renderComponent({ type: 'primary' });
+    expect(getByRole('button')).toHaveClass('btn', 'btn-primary');
   });
 });
